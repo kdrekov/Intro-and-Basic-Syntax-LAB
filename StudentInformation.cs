9aa090b99afdb6af685110d22377cc286ee88ff9
Koycho Drekov <kdrekov@gmail.com>
2022-05-18 10:07:31 +0300

    Add project files.

---
+++ StudentInformation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Intro_and_Basic_Syntax_LAB
+{
+    class StudentInformation
+    {
+        static void Main(string[] args)
+        {
+            var name = Console.ReadLine();
+            var age = Console.ReadLine();
+            var grade = Console.ReadLine();
+            Console.WriteLine("Name: {0}, Age: {1}, Grade: {2}", name, age, grade);
+        }
+    }
+}
